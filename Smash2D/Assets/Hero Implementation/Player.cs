7c8a4dde0b9f3ff4441d4e31108beaa940f87f6f
HRCK0 <proka2000@gmail.com>
2019-02-09 18:46:08 -0500

    Minor changes

--- Smash2D/Assets/Hero Implementation/Player.cs
+++ Smash2D/Assets/Hero Implementation/Player.cs
@@ -36,11 +36,11 @@ namespace Hero_Implementation
         {
             if (Input.GetKey(left))
             {
-                theRigidBody.velocity = new Vector2(-player.getMovingSpeed(), theRigidBody.velocity.y);
+                theRigidBody.velocity = new Vector2(-theRigidBody.velocity.x, theRigidBody.velocity.y);//-player.getMovingSpeed()
             }
             else if (Input.GetKey(right))
             {
-                theRigidBody.velocity = new Vector2(player.getMovingSpeed(), theRigidBody.velocity.y);
+                theRigidBody.velocity = new Vector2(theRigidBody.velocity.x, theRigidBody.velocity.y);
             }
             else
             {
