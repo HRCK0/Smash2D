ba00d1e012fcd21e0e6987659cef00cfab4efa21
Braden Collingwood <bradencollingwood82@gmail.com>
2019-02-09 19:07:27 -0500

    Test

---
+++ Smash2D/Assets/Hero Implementation/MageController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+
+    public Rigidbody2D theRB;
+
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatisGround;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatisGround);
+        if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        }
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale= new Vector3(-1, 1, 1);
+        }
+        else if(theRB.velocity.x > 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+}
