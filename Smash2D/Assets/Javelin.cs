c4063df6a224f0018bd0717d3ad81e5175600f5b
Gabriel Granata <ggran067@uottawa.ca>
2019-02-09 22:33:52 -0500

    added javelin + javelina effect (basic)

---
+++ Smash2D/Assets/Javelin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Javelin : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D theRB;
+
+    public GameObject javelinEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(speed * -transform.localScale.x, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(javelinEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
